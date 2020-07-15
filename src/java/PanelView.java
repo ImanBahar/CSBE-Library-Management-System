b50f3dfbbaa994938cb798ba7d6161cb323f2e0e
fatinnabilla <62294375+fatinnabilla@users.noreply.github.com>
2020-07-15 16:01:54 +0800

    Add files via upload

---
+++ src/java/PanelView.java
@@ -0,0 +1,32 @@
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import org.primefaces.event.CloseEvent;
+import org.primefaces.event.ToggleEvent;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+@Named
+@RequestScoped
+public class PanelView {
+     
+    public void onClose(CloseEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Panel Closed", "Closed panel id:'" + event.getComponent().getId() + "'");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+     
+    public void onToggle(ToggleEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, event.getComponent().getId() + " toggled", "Status:" + event.getVisibility().name());
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
